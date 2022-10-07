54927eb0ab56e48a97795e81c58ba59db0534435
Manraj-Mann <manrajmannhere@gmail.com>
2022-10-07 12:48:35 +0530

    added 3 sum

---
+++ 15.cpp
@@ -0,0 +1,90 @@
+/*Author : Manraj Singh */
+#include <bits/stdc++.h>
+using namespace std;
+
+// Problem : 	15. 3Sum
+/* Solution : 	( MOdified two Pointer Approach with removing duplicates ) */
+
+class Solution
+{
+public:
+    vector<vector<int>> threeSum(vector<int> &nums)
+    {
+
+        int n = nums.size();
+        vector<vector<int>> ans;
+
+        sort(nums.begin(), nums.end());
+
+        int i = 0;
+        int j = i + 1;
+        int k = n - 1;
+
+        while (i < n)
+        {
+            j = i + 1;
+            k = n - 1;
+            while (j < k)
+            {
+
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0)
+                {
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    printf("%d %d %d\n", nums[i], nums[j], nums[k]);
+
+                    while (j + 1 < k && nums[j] == nums[j + 1])
+                    {
+
+                        j++;
+                    }
+                    j++;
+                }
+                else if (sum < 0)
+                {
+
+                    j++;
+                }
+                else
+                {
+
+                    k--;
+                }
+            }
+
+            while (i + 1 < n && nums[i] == nums[i + 1])
+            {
+
+                i++;
+            }
+            
+
+            i++;
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+    vector<int> v;
+    while (n--)
+    {
+        int k;
+        cin >> k;
+        v.push_back(k);
+    }
+
+    Solution s;
+    s.threeSum(v);
+
+    return 0;
+}
