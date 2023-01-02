8535bb7f0632dfdce577e0a69a148f2a644b4409
Manraj-Mann <manrajmannhere@gmail.com>
2023-01-02 21:49:02 +0530

    520.py

---
+++ 520.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def detectCapitalUse(self, word):
+        
+        capcount = 0
+        for w in word:
+            if w.isupper():
+                capcount += 1
+        
+        if capcount == len(word):
+            return True
+        if capcount == 0:
+            return True
+        if capcount == 1 and word[0].isupper():
+            return True
+        
+        return False
+                    
+
+
+
